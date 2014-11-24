cd0719118bc7cf058c0ef3a847bc58f850786713
mrxordi <xordi@wp.pl>
2014-11-24 06:57:46 +0000

    Revert "Added OnDestroy function"
    This reverts commit 8eff3cb450e4c2e29cfb2e8831370ad5ae36bbdc.

--- XRTests/Application.cpp
+++ XRTests/Application.cpp
@@ -1,15 +1,11 @@
 #include "stdafx.h"
-#include <vld.h>
-
 #include "Application.h"
-#include "CurlJsonCConverterLoggerT.h"
-//#include "../XRFramework/log/Logger.h"
-//#include "../XRFramework/log/LoggerDAL.h"
-//#include "../XRFramework/log/LogDefines.h"
+#include "../XRFramework/log/Logger.h"
+#include "../XRFramework/log/LoggerDAL.h"
+#include "../XRFramework/log/LogDefines.h"
 #include "../XRFramework/window/WindowEvents.h"
 #include "../XRFramework/RenderControl.h"
 #include "../XRFramework/render/RenderSystemDX.h"
-
 
 
 template<> Application* Singleton<Application>::ms_Singleton = 0;
@@ -30,16 +26,12 @@ bool Application::Initialize(HINSTANCE hInstance, int nCmdShow) {
 	LoadString(hInstance, IDC_XRTESTS, szWindowClass, 100);
 	MyRegisterClass(hInstance);
 
-	cRenderSystemDX::Create();
-
 	// Perform application initialization:
 	if (!InitInstance(hInstance, nCmdShow))
 	{
 		LOGDEBUG("Failed to initialize window system.");
 		return false;
 	}
-	CurlJsonCharsetConverterLoggerTests();
-
 	return true;
 }
 
@@ -55,7 +47,7 @@ void Application::Run() {
 		g_DXRendererPtr->EndRender();
 
 	}
-	OnDestroy();
+	g_DXRendererPtr->DestroyRenderSystem();
 }
 
 bool Application::AppMsgProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
@@ -87,16 +79,11 @@ bool Application::OnCreate(HWND hWnd) {
 	//window creation
 	RECT rc = { 20, 20, 400, 250 };
 	m_rendercontrol.Initialize(hWnd, hInst, rc);
+	cRenderSystemDX::Create();
 
 	g_DXRendererPtr->InitRenderSystem(&m_rendercontrol);
 
 
-	return true;
-}
-
-bool Application::OnDestroy() {
-	g_DXRendererPtr->DestroyRenderSystem();
-	g_DXRendererPtr->Destroy();
 	return true;
 }
 
--- XRTests/XRTests.cpp
+++ XRTests/XRTests.cpp
@@ -4,22 +4,44 @@
 #include "stdafx.h"
 #include "XRTests.h"
 #include "Application.h"
-#include "DLLoader/LoaderFactory.h"
-#include "filesystem/DllLibCurl.h"
 #include "../XRFramework/filesystem/SpecialProtocol.h"
 #include "../XRFramework/log/Log.h"
 #include "../XRFramework/Util.h"
-#include "../XRFramework/utils/ConverterFactory.h"
 
 int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPTSTR    lpCmdLine,
 	_In_ int       nCmdShow)
 {
-	UNREFERENCED_PARAMETER(hPrevInstance);
+	/*UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
 
+	// TODO: Place code here.
+	MSG msg;
+	HACCEL hAccelTable;
 
+	// Initialize global strings
+	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadString(hInstance, IDC_XRTESTS, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
+
+	// Perform application initialization:
+	if (!InitInstance(hInstance, nCmdShow))
+	{
+	return FALSE;
+	}
+
+	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_XRTESTS));
+
+	// Main message loop:
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+	if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+	{
+	TranslateMessage(&msg);
+	DispatchMessage(&msg);
+	}
+	}*/
 
 	CStdString appPath, tempPath, docPath, homePath;
 
@@ -37,33 +59,148 @@ int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
 
 	DWORD m_threadid = GetCurrentThreadId();
 
-	CLog::Create();
-
-	if (!g_LogPtr->Init(std::string("special://app/log.txt")))
+	if (!Logger::Init("special://app/log.txt"))
 	{
 		fprintf(stderr, "Could not init logging classes. Permission errors on (%s)\n",
 			CSpecialProtocol::TranslatePath("special://home/").c_str());
 		return false;
 	}
-	g_LogPtr->SetExtraLogLevels(LOGCURL);
 
-	LOGINFO("Start application initialization. Running on thread: %d", m_threadid);
+	LOGDEBUG("cApplication Initialization. App Running on thread: %d", m_threadid);
 
 	CSpecialProtocol::LogPaths();
 
-	Application::Create();
-
+	Application app;
 	g_applicationPtr->Initialize(hInstance, nCmdShow);
 	g_applicationPtr->Run();
-
-	Application::Destroy();
-
-	DllLibCurlGlobal::Get().UnloadAll();
-
-
-	CLog::Destroy();
-	ConverterFactory::DestroyAll();
 
 
 	return (int)0;
 }
+
+
+/*
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+WNDCLASSEX wcex;
+
+wcex.cbSize = sizeof(WNDCLASSEX);
+
+wcex.style = CS_HREDRAW | CS_VREDRAW;
+wcex.lpfnWndProc = WndProc;
+wcex.cbClsExtra = 0;
+wcex.cbWndExtra = 0;
+wcex.hInstance = hInstance;
+wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_XRTESTS));
+wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+wcex.lpszMenuName = MAKEINTRESOURCE(IDC_XRTESTS);
+wcex.lpszClassName = szWindowClass;
+wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+return RegisterClassEx(&wcex);
+}
+
+//
+//   FUNCTION: InitInstance(HINSTANCE, int)
+//
+//   PURPOSE: Saves instance handle and creates main window
+//
+//   COMMENTS:
+//
+//        In this function, we save the instance handle in a global variable and
+//        create and display the main program window.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+HWND hWnd;
+
+hInst = hInstance; // Store instance handle in our global variable
+
+hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+
+if (!hWnd)
+{
+return FALSE;
+}
+
+ShowWindow(hWnd, nCmdShow);
+UpdateWindow(hWnd);
+
+return TRUE;
+}
+
+//
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE:  Processes messages for the main window.
+//
+//  WM_COMMAND	- process the application menu
+//  WM_PAINT	- Paint the main window
+//  WM_DESTROY	- post a quit message and return
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+int wmId, wmEvent;
+PAINTSTRUCT ps;
+HDC hdc;
+
+switch (message)
+{
+case WM_COMMAND:
+wmId = LOWORD(wParam);
+wmEvent = HIWORD(wParam);
+// Parse the menu selections:
+switch (wmId)
+{
+case IDM_ABOUT:
+DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+break;
+case IDM_EXIT:
+DestroyWindow(hWnd);
+break;
+default:
+return DefWindowProc(hWnd, message, wParam, lParam);
+}
+break;
+case WM_PAINT:
+hdc = BeginPaint(hWnd, &ps);
+// TODO: Add any drawing code here...
+EndPaint(hWnd, &ps);
+break;
+case WM_DESTROY:
+PostQuitMessage(0);
+break;
+default:
+return DefWindowProc(hWnd, message, wParam, lParam);
+}
+return 0;
+}
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+UNREFERENCED_PARAMETER(lParam);
+switch (message)
+{
+case WM_INITDIALOG:
+return (INT_PTR)TRUE;
+
+case WM_COMMAND:
+if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+{
+EndDialog(hDlg, LOWORD(wParam));
+return (INT_PTR)TRUE;
+}
+break;
+}
+return (INT_PTR)FALSE;
+}
+*/
