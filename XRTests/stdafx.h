63eb78f0a58a65a721d597e7900c43a329574e5f
mrxordi <xordi@wp.pl>
2014-11-24 06:56:26 +0000

    Json impl

--- XRTests/stdafx.h
+++ XRTests/stdafx.h
@@ -6,7 +6,7 @@
 #pragma once
 
 #include "targetver.h"
-
+#define JSON_DLL_BUILD
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files:
 #include <windows.h>
