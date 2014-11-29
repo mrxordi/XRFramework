20266b2a271f7aa1a383f5bf6426f6854e3beba6
mrxordi <xordi@wp.pl>
2014-11-29 19:27:41 +0000

    Date Time representation class

--- XRThreads/SystemClock.cpp
+++ XRThreads/SystemClock.cpp
@@ -20,12 +20,47 @@
 #undef max
 
 #include "stdafx.h"
-#include <stdint.h>
 #include <windows.h>
 #include "SystemClock.h"
+#include "../XRFramework/utils/DateTime.h"
 
 namespace XR
 {
+
+	int64_t CurrentHostCounter(void)
+	{
+		LARGE_INTEGER PerformanceCount;
+		QueryPerformanceCounter(&PerformanceCount);
+		return((int64_t)PerformanceCount.QuadPart);
+	}
+
+	int64_t CurrentHostFrequency(void)
+	{
+		LARGE_INTEGER Frequency;
+		QueryPerformanceFrequency(&Frequency);
+		return((int64_t)Frequency.QuadPart);
+	}
+
+	CDateTime GetLocalTime(time_t time)
+	{
+		CDateTime result;
+
+		tm *local;
+
+		local = localtime(&time); // Conversion to local time
+
+		/*
+		* Microsoft implementation of localtime returns NULL if on or before epoch.
+		* http://msdn.microsoft.com/en-us/library/bf12f0hc(VS.80).aspx
+		*/
+		if (local)
+			result = *local;
+		else
+			result = time; // Use the original time as close enough.
+
+		return result;
+	}
+
 	unsigned int SystemClockMillis()
 	{
 		uint64_t now_time;
