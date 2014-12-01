10a253d5e24667d9cb5555b0203d9c44a70fd738
mrxordi <xordi@wp.pl>
2014-12-01 01:37:20 +0000

    File operations handling in background.

---
+++ XRFramework/filesystem/FileOperationJob.h
@@ -0,0 +1,80 @@
+#pragma once
+
+#include "FileItem.h"
+#include "utils/Job.h"
+#include "filesystem/File.h"
+#include "IApplication.h"
+#include "utils/IJobCallback.h"
+
+
+class FileOperationJob : public CJob, public IJobCallback
+{
+public:	
+	enum FileAction
+	{
+		ActionCopy = 1,
+		ActionMove,
+		ActionDelete,
+		ActionReplace, ///< Copy, emptying any existing destination directories first
+		ActionCreateFolder,
+		ActionDeleteFolder,
+	};
+
+	virtual void OnJobComplete(unsigned int jobID, bool success, CJob *job) {};
+
+	FileOperationJob();
+
+	FileOperationJob(FileAction action, FileItemList & items,
+		const CStdString& strDestFile,
+		bool displayProgress = false);
+
+	void SetFileOperation(FileAction action, FileItemList &items, const CStdString &strDestFile);
+
+	virtual bool DoWork();
+
+	static std::string GetActionString(FileAction action);
+	const char* GetType() const { return m_bDisplayProgress ? "filemanager" : ""; }
+	const std::string &GetAverageSpeed()     const { return m_avgSpeed; }
+	const std::string &GetCurrentOperation() const { return m_currentOperation; }
+	const std::string &GetCurrentFile()      const { return m_currentFile; }
+	const FileItemList &GetItems()         const { return m_items; }
+	FileAction GetAction() const            { return m_fileaction; }
+
+
+	virtual bool operator==(const CJob *job) const;
+	
+private:
+	class FileOperation : public IFileCallback
+	{
+	public:
+		FileOperation(FileAction action, const CStdString &strFileA, const CStdString &strFileB, int64_t time);
+		bool ExecuteOperation(FileOperationJob *base, double &current, double opWeight);
+		void Debug();
+		virtual bool OnFileCallback(void* pContext, int ipercent, float avgSpeed);
+	private:
+		FileAction m_action;
+		CStdString m_strFileA, m_strFileB;
+		int64_t m_time;
+	};
+
+	friend class FileOperation;
+	typedef std::vector<FileOperation> FileOperationList;
+
+	bool DoProcess(FileAction action, FileItemList& items, const std::string& strDestFile, FileOperationList &fileOperations, double &totalTime);
+	bool DoProcessFolder(FileAction action, const std::string& strPath, const std::string& strDestFile, FileOperationList &fileOperations, double &totalTime);
+	bool DoProcessFile(FileAction action, const std::string& strFileA, const std::string& strFileB, FileOperationList &fileOperations, double &totalTime);
+
+	static inline bool CanBeRenamed(const CStdString &strFileA, const CStdString &strFileB);
+
+private:
+	StatusBarPtr m_hStatusBar;
+	bool		m_bDisplayProgress;
+	FileAction	m_fileaction;
+
+	std::string m_strDestFile,
+				m_avgSpeed, 
+				m_currentOperation, 
+				m_currentFile;
+	FileItemList m_items;
+};
+
