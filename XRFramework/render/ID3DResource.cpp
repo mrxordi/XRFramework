7109d33b99dfd0a7e4f802a5dcef79f345f46b16
mrxordi <xordi@wp.pl>
2014-12-14 22:15:04 +0000

    Texture and effect impl

--- XRFramework/render/ID3DResource.cpp
+++ XRFramework/render/ID3DResource.cpp
@@ -1,3 +1,420 @@
+m_resource
+m_resource
+m_renderTargetView
+m_renderTargetView
 #include "stdafxf.h"
 #include "ID3DResource.h"
+#include "RenderSystemDX.h"
+#include "Base.h"
 
+const char* usages[] = { "D3D10_USAGE_DEFAULT", "D3D10_USAGE_IMMUTABLE", "D3D10_USAGE_DYNAMIC", "D3D10_USAGE_STAGING" };
+
+/************************************************************************/
+/*    D3DTexture                                                        */
+/************************************************************************/
+D3DTexture::D3DTexture()
+{
+	m_bLocked = false;
+	m_resource = nullptr;
+	m_shaderResourceView = nullptr;
+	m_renderTargetView = nullptr;
+}
+
+D3DTexture::~D3DTexture()
+{
+}
+
+bool D3DTexture::Create(UINT width, UINT height, D3D10_BIND_FLAG bindflag, D3D10_USAGE usage, DXGI_FORMAT format, UINT mipLevels)
+{
+	m_mipLevels = mipLevels;
+	m_format = format;
+	m_bindflag = bindflag;
+	ID3D10Device* pDevice = g_DXRendererPtr->GetDevice();
+
+	DXGI_SAMPLE_DESC dxgiSampleDesc;
+	dxgiSampleDesc.Count = 1;
+	dxgiSampleDesc.Quality = 0;
+
+	D3D10_TEXTURE2D_DESC d3d10Texture2DDesc;
+	ZeroMemory(&d3d10Texture2DDesc, sizeof(d3d10Texture2DDesc));
+
+	d3d10Texture2DDesc.Width = width;
+	d3d10Texture2DDesc.Height = height;
+	d3d10Texture2DDesc.MipLevels = mipLevels;
+	d3d10Texture2DDesc.ArraySize = 1;
+	d3d10Texture2DDesc.Format = format;
+	d3d10Texture2DDesc.SampleDesc = dxgiSampleDesc;
+	d3d10Texture2DDesc.Usage = usage;
+	d3d10Texture2DDesc.BindFlags = bindflag;
+	d3d10Texture2DDesc.CPUAccessFlags = (usage == D3D10_USAGE_DYNAMIC) ? D3D10_CPU_ACCESS_WRITE : 0;
+	d3d10Texture2DDesc.MiscFlags = (mipLevels > 0) ? D3D10_RESOURCE_MISC_GENERATE_MIPS : 0;
+
+	if (!g_DXRenderer.getSingletonPtr()) {
+		LOGERR("Failed to create texture!");
+		return false;
+	}
+
+	HRESULT hr = pDevice->CreateTexture2D(&d3d10Texture2DDesc, NULL, &m_resource);
+	if (FAILED(hr))
+	{
+		LOGERR("Failed to create texture: 0x%08X", hr)
+	}
+	else {
+		D3D10_TEXTURE2D_DESC desc;
+		m_resource->GetDesc(&desc);
+
+		if ((m_bindflag & D3D10_BIND_SHADER_RESOURCE) == D3D10_BIND_SHADER_RESOURCE) {
+
+			if (desc.Format != m_format)
+				LOGWARN("- format changed from %d to %d", m_format, desc.Format);
+			if (desc.Height != m_height || desc.Width != m_width)
+				LOGWARN("- size changed from %ux%u to %ux%u", m_width, m_height, desc.Width, desc.Height);
+
+			D3D10_SHADER_RESOURCE_VIEW_DESC rtDesc;
+			rtDesc.Format = d3d10Texture2DDesc.Format;
+			rtDesc.ViewDimension = D3D10_SRV_DIMENSION_TEXTURE2D;
+			rtDesc.Texture2D.MostDetailedMip = 0;
+			rtDesc.Texture2D.MipLevels = mipLevels;
+			HR(pDevice->CreateShaderResourceView(m_resource, &rtDesc, &m_shaderResourceView));
+
+			if (!m_shaderResourceView) {
+				LOGERR("Failed to create Shader Resource View!");
+				return false;
+			}
+
+			LOGINFO("Texture dimension %ux%u created with usage: %s", m_width, m_height, usages[usage]);
+		}
+		else if ((m_bindflag & D3D10_BIND_RENDER_TARGET) == D3D10_BIND_RENDER_TARGET) {
+			D3D10_RENDER_TARGET_VIEW_DESC rtDesc;
+			rtDesc.Format = desc.Format;
+			rtDesc.ViewDimension = D3D10_RTV_DIMENSION_TEXTURE2D;
+			rtDesc.Texture2D.MipSlice = 0;
+			HR(pDevice->CreateRenderTargetView(m_resource, &rtDesc, &m_renderTargetView));
+
+			if (!m_renderTargetView) {
+				LOGERR("Failed to create Shader Render Target View!");
+				return false;
+			}
+		} else {
+			LOGERR("Unsupported Bind flags for texture resource!");
+			Release();
+			return false;
+		}
+
+		g_DXRendererPtr->Register(this);
+		return true;
+	}
+	return false;
+}
+
+bool D3DTexture::Lock(UINT level, D3D10_MAP typeofmap, D3D10_MAPPED_TEXTURE2D & mappedtexture)
+{
+	if (m_bLocked)
+		return false;
+
+	bool bResult = (m_resource->Map(D3D10CalcSubresource(0, 0, level), typeofmap, 0, &mappedtexture) >= 0);
+	if (bResult)
+		m_bLocked = true;
+
+	return m_bLocked;
+}
+
+bool D3DTexture::Unlock(UINT level)
+{
+	if (!m_bLocked)
+		return false;
+
+	m_resource->Unmap(D3D10CalcSubresource(0, 0, level));
+	m_bLocked = false;
+	return true;
+}
+
+void D3DTexture::Release()
+{
+	SAFE_RELEASE(m_shaderResourceView);
+	SAFE_RELEASE(m_renderTargetView);
+	SAFE_RELEASE(m_resource);
+	g_DXRendererPtr->Unregister(this);
+}
+
+void D3DTexture::OnDestroyDevice()
+{
+	SAFE_RELEASE(m_shaderResourceView);
+	SAFE_RELEASE(m_renderTargetView);
+	SAFE_RELEASE(m_resource);
+}
+
+/************************************************************************/
+/*    D3DEffect                                                         */
+/************************************************************************/
+D3DEffect::D3DEffect()
+{
+	m_effect = NULL;
+	m_technique = NULL;
+	m_defines.clear();
+}
+
+D3DEffect::~D3DEffect()
+{
+}
+
+bool D3DEffect::Create(const std::string &effectString, DefinesMap* defines)
+{
+	if (m_effect)
+		return false;
+
+	m_effectString = effectString;
+	m_defines.clear();
+	if (defines != NULL)
+		m_defines = *defines; //FIXME: is this a copy of all members?
+	if (CreateEffect())
+	{
+		g_DXRendererPtr->Register(this);
+		return true;
+	}
+
+	SAFE_RELEASE(m_effect);
+	m_defines.clear();
+	return false;
+}
+
+void D3DEffect::Release()
+{
+	SAFE_RELEASE(m_effect);
+	m_defines.clear();
+	g_DXRendererPtr->Unregister(this);
+}
+
+bool D3DEffect::SetFloatArray(std::string handle, const float* val, unsigned int count)
+{
+	if (m_effect)
+		return (m_effect->GetVariableByName(handle.c_str())->SetRawValue((void*)val, 0, count) >= 0);
+	return false;
+}
+
+bool D3DEffect::SetMatrix(std::string handle, XMMATRIX& mat)
+{
+	if (m_effect)
+		return (m_effect->GetVariableByName(handle.c_str())->AsMatrix()->SetMatrix(reinterpret_cast<float*>(&mat)) >= 0);
+
+	return false;
+}
+
+bool D3DEffect::SetTechnique(std::string handle)
+{
+	if (m_effect)
+		m_technique = m_effect->GetTechniqueByName(handle.c_str());
+	return (m_technique != 0);
+}
+
+bool D3DEffect::SetTexture(std::string handle, D3DTexture &texture)
+{
+	if (m_effect)
+		return (m_effect->GetVariableByName(handle.c_str())->AsShaderResource()->SetResource(texture.Get()) >= 0);
+	return false;
+}
+
+bool D3DEffect::BeginPass(UINT pass)
+{
+	return (m_technique->GetPassByIndex(pass)->Apply(0) >= 0);
+}
+
+void D3DEffect::OnDestroyDevice()
+{
+	SAFE_RELEASE(m_effect);
+	m_defines.clear();
+}
+
+bool D3DEffect::CreateEffect()
+{
+	if (m_effect)
+		return false;
+	std::vector<D3D10_SHADER_MACRO> definemacros;
+
+	for (DefinesMap::const_iterator it = m_defines.begin(); it != m_defines.end(); ++it)
+	{
+		D3D10_SHADER_MACRO m;
+		m.Name = it->first.c_str();
+		if (it->second.empty())
+			m.Definition = NULL;
+		else
+			m.Definition = it->second.c_str();
+		definemacros.push_back(m);
+	}
+
+	definemacros.push_back(D3D10_SHADER_MACRO());
+	definemacros.back().Name = 0;
+	definemacros.back().Definition = 0;
+
+	// create the main effect from the shader source.
+	ID3D10Blob* errors = 0;
+	//if (FAILED(D3DX10CreateEffectFromMemory(shaderSource, sizeof(shaderSource),
+	//	0, 0, 0, "fx_4_0", 0, 0, d_device,
+	//	0, 0, &d_effect, &errors, 0)))
+	//{
+	//	std::string msg(static_cast<const char*>(errors->GetBufferPointer()),
+	//		errors->GetBufferSize());
+	//	errors->Release();
+	//	CEGUI_THROW(RendererException(msg));
+	//}
+	ID3D10Blob* compiledeffect = 0;
+	UINT HSLSflags = 0;
+	HSLSflags |= D3D10_SHADER_ENABLE_STRICTNESS;
+#ifdef _DEBUG
+	HSLSflags |= D3D10_SHADER_DEBUG;
+#endif
+	if (FAILED(D3D10CompileEffectFromMemory((void*)m_effectString.c_str(), m_effectString.length(), 0, &definemacros[0], 0, D3D10_SHADER_ENABLE_STRICTNESS, 0, &compiledeffect, &errors)))
+	{
+		std::string msg(static_cast<const char*>(errors->GetBufferPointer()),
+			errors->GetBufferSize());
+		LOGERR("%s", msg.c_str());
+		errors->Release();
+		compiledeffect->Release();
+		return false;
+	}
+
+	if (FAILED(D3D10CreateEffectFromMemory(compiledeffect->GetBufferPointer(), compiledeffect->GetBufferSize(), 0, g_DXRendererPtr->GetDevice(), 0, &m_effect)))
+	{
+		std::string msg(static_cast<const char*>(errors->GetBufferPointer()),
+			errors->GetBufferSize());
+		LOGERR("%s", msg.c_str());
+		errors->Release();
+		compiledeffect->Release();
+		return false;
+	}
+	compiledeffect->Release();
+
+	return true;
+}
+
+D3DVertexBuffer::D3DVertexBuffer()
+{
+	m_bIsValid = false;
+	m_length = 0;
+	m_indexBuffer = NULL;
+	m_type = NONINDEXED_BUFFER;
+	m_indexBuffer = NULL;
+}
+
+D3DVertexBuffer::~D3DVertexBuffer()
+{
+}
+
+bool D3DVertexBuffer::CreateBuffer(UINT length, D3D10_USAGE usage /*= D3D10_USAGE_DEFAULT*/, void* data /*= NULL*/)
+{
+	if (m_vertexBuffer)
+		return false;
+
+	if (usage == D3D10_USAGE_IMMUTABLE && data == NULL && length == 0) {
+		LOGERR("No data provided while creating %s buffer", usages[usage]);
+		return false;
+	}
+	D3D10_BUFFER_DESC vertexBufferDesc;
+
+	// Set up the description of the vertex buffer.
+	vertexBufferDesc.Usage = usage;
+	vertexBufferDesc.ByteWidth = length;
+	vertexBufferDesc.BindFlags = D3D10_BIND_VERTEX_BUFFER;
+	vertexBufferDesc.CPUAccessFlags = (usage == D3D10_USAGE_DYNAMIC) ? D3D10_CPU_ACCESS_WRITE : 0;
+	vertexBufferDesc.MiscFlags = 0;
+
+	D3D10_SUBRESOURCE_DATA InitData;
+	if (data) {
+		ZeroMemory(&InitData, sizeof(InitData));
+		InitData.pSysMem = data;
+	}
+
+	HRESULT hhr = -1L;
+
+	if (g_DXRendererPtr->GetDevice()) {
+		hhr = g_DXRendererPtr->GetDevice()->CreateBuffer(&vertexBufferDesc, data ? &InitData : NULL, &m_vertexBuffer);
+	}
+	if (SUCCEEDED(hhr)) {
+		m_length = length;
+		m_bIsValid = true;
+		g_DXRendererPtr->Register(this);
+		return true;
+	}
+	else
+		HR(hhr);
+
+	SAFE_RELEASE(m_vertexBuffer);
+	return false;
+}
+
+bool D3DVertexBuffer::CreateIndex(UINT length, D3D10_USAGE usage /*= D3D10_USAGE_DEFAULT*/, void* data /*= NULL*/)
+{
+	if (m_indexBuffer)
+		return false;
+
+	if (usage == D3D10_USAGE_IMMUTABLE && data == NULL) {
+		LOGERR("No data provided while creating %s index buffer", usages[usage]);
+		return false;
+	}
+	D3D10_BUFFER_DESC indexBufferDesc;
+
+	// Set up the description of the vertex buffer.
+	indexBufferDesc.Usage = usage;
+	indexBufferDesc.ByteWidth = length;
+	indexBufferDesc.BindFlags = D3D10_BIND_INDEX_BUFFER;
+	indexBufferDesc.CPUAccessFlags = (usage == D3D10_USAGE_DYNAMIC) ? D3D10_CPU_ACCESS_WRITE : 0;
+	indexBufferDesc.MiscFlags = 0;
+
+	D3D10_SUBRESOURCE_DATA InitData;
+	if (data) {
+		ZeroMemory(&InitData, sizeof(InitData));
+		InitData.pSysMem = data;
+	}
+
+	HRESULT hhr = -1;
+	if (g_DXRendererPtr->GetDevice()) {
+		hhr = g_DXRendererPtr->GetDevice()->CreateBuffer(&indexBufferDesc, data ? &InitData : NULL, &m_indexBuffer);
+	}
+	if (SUCCEEDED(hhr)) {
+		m_type = INDEXED_BUFFER;
+		return true;
+	}
+	else
+		HR(hhr);
+
+	SAFE_RELEASE(m_indexBuffer);
+	m_type = NONINDEXED_BUFFER;
+	return false;
+}
+
+void D3DVertexBuffer::Release()
+{
+	SAFE_RELEASE(m_vertexBuffer);
+	SAFE_RELEASE(m_indexBuffer);
+	m_type = NONINDEXED_BUFFER;
+	m_bIsValid = false;
+	g_DXRendererPtr->Unregister(this);
+}
+
+bool D3DVertexBuffer::LockBuffer(D3D10_MAP mapType, void **data)
+{
+	return (m_vertexBuffer->Map(D3D10_MAP_WRITE_DISCARD, NULL, data) >= 0);
+}
+
+void D3DVertexBuffer::UnlockBuffer()
+{
+	m_vertexBuffer->Unmap();
+}
+
+bool D3DVertexBuffer::LockIndex(D3D10_MAP mapType, void **data)
+{
+	return (m_indexBuffer->Map(D3D10_MAP_WRITE_DISCARD, NULL, data) >= 0);
+}
+
+void D3DVertexBuffer::UnlockIndex()
+{
+	m_indexBuffer->Unmap();
+}
+
+void D3DVertexBuffer::OnDestroyDevice()
+{
+	SAFE_RELEASE(m_vertexBuffer);
+	SAFE_RELEASE(m_indexBuffer);
+	m_type = NONINDEXED_BUFFER;
+	m_bIsValid = false;
+}
--- XRFramework/render/ID3DResource.h
+++ XRFramework/render/ID3DResource.h
@@ -1,4 +1,10 @@
 #pragma once
+#include <map>
+#include <d3d10.h>
+#include <DirectXMath.h>
+#include <string>
+using namespace DirectX;
+
 class ID3DResource
 {
 public:
@@ -10,13 +16,114 @@ public:
 	virtual void OnResizeDevice() {};
 };
 
-class Texture : public ID3DResource
+/************************************************************************/
+/*    D3DTexture                                                        */
+/************************************************************************/
+class D3DTexture : public ID3DResource
 {
 public:
-	Texture(){};
-	~Texture(){};
+	D3DTexture();
+	~D3DTexture();
 
-	void OnDestroyDevice(){};
-	void OnCreateDevice(){};
-	void OnResizeDevice(){};
+	bool Create(UINT width, UINT height, D3D10_BIND_FLAG bindflag, D3D10_USAGE usage, DXGI_FORMAT format, UINT mipLevels = 1);
+	void Release();
+
+	bool Lock(UINT level, D3D10_MAP typeofmap, D3D10_MAPPED_TEXTURE2D & mappedtexture);
+	bool Unlock(UINT level);
+
+	ID3D10ShaderResourceView* Get() { return m_shaderResourceView; };
+	UINT GetWidth()  const { return m_width; }
+	UINT GetHeight() const { return m_height; }
+	DXGI_FORMAT GetFormat() const { return m_format; }
+
+	virtual void OnDestroyDevice() override;
+private:
+	D3D10_BIND_FLAG m_bindflag;
+	// creation parameters
+	UINT      m_width;
+	UINT      m_height;
+	UINT      m_mipLevels;
+	DXGI_FORMAT m_format;
+
+	ID3D10Texture2D* m_resource;
+	ID3D10ShaderResourceView* m_shaderResourceView;
+	ID3D10RenderTargetView* m_renderTargetView;
+	
+	bool m_bLocked;
+};
+
+/************************************************************************/
+/*    D3DEffect                                                         */
+/************************************************************************/
+typedef std::map<std::string, std::string> DefinesMap;
+
+class D3DEffect : public ID3DResource
+{
+public:
+	D3DEffect();
+	virtual ~D3DEffect();
+
+	bool Create(const std::string &effectString, DefinesMap* defines);
+	void Release();
+
+	bool SetFloatArray(std::string handle, const float* val, unsigned int count);
+	bool SetMatrix(std::string handle, XMMATRIX& mat);
+	bool SetTechnique(std::string handle);
+	bool SetTexture(std::string handle, D3DTexture &texture);
+
+	//bool Begin(UINT *passes, DWORD flags);
+	bool BeginPass(UINT pass);
+	//bool EndPass();
+	//bool End();
+	ID3D10EffectTechnique* GetTechnique() { if (m_technique) return m_technique; return NULL; }
+	ID3D10Effect* Get() { return m_effect; }
+
+	virtual void OnDestroyDevice() override;
+
+private:
+	bool         CreateEffect();
+	std::string  m_effectString;
+
+	//! Effect (shader) used when rendering.
+	ID3D10Effect* m_effect;
+	ID3D10EffectTechnique* m_technique;
+	DefinesMap   m_defines;
+};
+
+/************************************************************************/
+/*    D3DVertexBuffer                                                   */
+/************************************************************************/
+class D3DVertexBuffer : public ID3DResource
+{
+public:
+	enum type {
+		NONINDEXED_BUFFER,
+		INDEXED_BUFFER
+	};
+
+	D3DVertexBuffer();
+	virtual ~D3DVertexBuffer();
+
+	bool CreateBuffer(UINT length, D3D10_USAGE usage = D3D10_USAGE_DEFAULT, void* data = NULL);
+	bool CreateIndex(UINT length, D3D10_USAGE usage = D3D10_USAGE_DEFAULT, void* data = NULL);
+	void Release();
+
+	bool LockBuffer(D3D10_MAP mapType, void **data);
+	void UnlockBuffer();
+	bool LockIndex(D3D10_MAP mapType, void **data);
+	void UnlockIndex();
+
+	ID3D10Buffer* GetVertexBuffer() { return m_vertexBuffer; };
+	ID3D10Buffer* GetIndexBuffer() const { return m_indexBuffer; };
+
+	virtual void OnDestroyDevice() override;
+
+	type		m_type;
+
+private:
+
+	bool		m_bIsValid;
+	UINT		m_length;
+	ID3D10Buffer* m_indexBuffer;
+	ID3D10Buffer* m_vertexBuffer;
 };
