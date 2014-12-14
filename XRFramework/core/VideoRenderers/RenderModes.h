a6f069c0078f4de43e944b46c8c51024779ed777
mrxordi <xordi@wp.pl>
2014-12-14 22:19:27 +0000

    Render Modes

---
+++ XRFramework/core/VideoRenderers/RenderModes.h
@@ -0,0 +1,21 @@
+#pragma once
+
+//Thats all for now, i'm not expecting to implement more before leaving beta stage
+
+enum ERenderFormat {
+	RENDER_FMT_NONE = 0,
+	RENDER_FMT_YUV420P,
+	RENDER_FMT_YUV420P10,
+	RENDER_FMT_YUV420P16,
+	RENDER_FMT_NV12,
+	RENDER_FMT_UYVY422,
+	RENDER_FMT_YUYV422,
+	//RENDER_FMT_DXVA,
+	//RENDER_FMT_OMXEGL,
+	//RENDER_FMT_CVBREF,
+	//RENDER_FMT_BYPASS,
+	//RENDER_FMT_EGLIMG,
+	//RENDER_FMT_MEDIACODEC,
+	//RENDER_FMT_IMXMAP,
+	//RENDER_FMT_MMAL,
+};
