3ab7213fce922c79720c7e170e60d2cc039303c5
mrxordi <xordi@wp.pl>
2014-12-24 06:20:47 +0000

    Important change All threads go to XR namespace

--- XRCommon/stdafx.h
+++ XRCommon/stdafx.h
@@ -5,10 +5,9 @@
 
 #pragma once
 
-#define NOMINMAX
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 #define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS      // some CString constructors will be explicit
-#define _AFX_NO_MFC_CONTROLS_IN_DIALOGS         // remove support for MFC controls in dialogs
+//#define _AFX_NO_MFC_CONTROLS_IN_DIALOGS         // remove support for MFC controls in dialogs
 
 #ifndef VC_EXTRALEAN
 #define VC_EXTRALEAN            // Exclude rarely-used stuff from Windows headers
@@ -19,9 +18,7 @@
 #include <string>
 #include <algorithm>
 #include <functional>
-#include "log/Log.h"
-
-
-
+#include <d3d10.h>
+#include <dxgi.h>
 
 // TODO: reference additional headers your program requires here
