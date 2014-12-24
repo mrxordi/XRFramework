734bde6d010d902394186a8a1e482f6d933178a5
mrxordi <xordi@wp.pl>
2014-12-24 00:30:08 +0000

    Entry to testing MFC

---
+++ XRTestMFC/XRTestMFC.cpp
@@ -0,0 +1,16 @@
+// XRTestMFC.cpp : Defines the entry point for the application.
+//
+
+#include "stdafx.h"
+#include "XRTestMFC.h"
+
+
+int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPTSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+
+
+	return (int) 0;
+}
---
+++ XRTestMFC/stdafx.h
@@ -0,0 +1,26 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files:
+#include <afxwin.h>                         // MFC core and standard components
+#include <afxext.h>                         // MFC extensions
+#include <afxdisp.h>                        // MFC Automation classes
+#include <afxdtctl.h>                       // MFC support for Internet Explorer 4 Common Controls
+#ifndef _AFX_NO_AFXCMN_SUPPORT
+#include <afxcmn.h>                         // MFC support for Windows Common Controls
+#endif // _AFX_NO_AFXCMN_SUPPORT
+#include <afxdlgs.h>
+
+// C RunTime Header Files
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+
+// TODO: reference additional headers your program requires here
