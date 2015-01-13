f61b4e302fbaf1ba7b319b5c69e1e8efa7903ee3
mrxordi <xordi@wp.pl>
2015-01-13 20:36:36 +0000

    SetThreadName

--- XRCommon/Util.cpp
+++ XRCommon/Util.cpp
@@ -195,3 +195,33 @@ bool CUtil::GetFocussedProcess(std::wstring &strProcessFile)
 
 	return true;
 }
+
+
+//
+// Usage: SetThreadName (-1, "MainThread");
+// Code from http://msdn.microsoft.com/en-us/library/xcb2z8hs%28v=vs.110%29.aspx
+//
+
+#pragma pack(push,8)
+typedef struct tagTHREADNAME_INFO {
+	DWORD  dwType;      // must be 0x1000
+	LPCSTR szName;      // pointer to name (in user addr space)
+	DWORD  dwThreadID;  // thread ID (-1 caller thread)
+	DWORD  dwFlags;     // reserved for future use, must be zero
+} THREADNAME_INFO;
+#pragma pack(pop)
+
+void SetThreadName(DWORD dwThreadID, LPCSTR szThreadName)
+{
+	THREADNAME_INFO info;
+	info.dwType = 0x1000;
+	info.szName = szThreadName;
+	info.dwThreadID = dwThreadID;
+	info.dwFlags = 0;
+
+	__try {
+		RaiseException(0x406D1388, 0, sizeof(info) / sizeof(ULONG_PTR), (ULONG_PTR*)&info);
+	}
+	__except (EXCEPTION_EXECUTE_HANDLER) {
+	}
+}
