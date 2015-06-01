c901f0fbac3c5ffa585b506caf9418ae74ce4b3f
Bartosz Slawinski <xordi@wp.pl>
2015-06-01 22:16:02 +0100

    Context implementation

---
+++ XRFwx/Context.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <memory>
+#include "XRFramework/render/D3D10Enumeration.h"
+#include "XRCommon/settings/helpers/Monitors.h"
+#include "wxDX10Display.h"
+class MainFrame;
+class CDX10SystemRenderer;
+class CDX10FrameRenderer;
+class MainFrame;
+class wxWindow;
+
+struct Context
+{
+	std::unique_ptr<CMonitors> monitors;
+
+	// Things that should probably be in some sort of UI-context-model
+	wxWindow *parent = nullptr;
+	wxWindow *previousFocus = nullptr;
+	wxWindow *videoSlider = nullptr;
+
+	MainFrame *frame = nullptr;
+	wxDX10Display *videoDisplay = nullptr;
+
+	Context()  : monitors(std::make_unique<CMonitors>()) {};
+	~Context() {};
+};
+
